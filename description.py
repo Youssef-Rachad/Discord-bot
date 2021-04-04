9866861c668624828467b02cb25908ec3c092476
Youssef-Rachad <69124029+Youssef-Rachad@users.noreply.github.com>
2021-04-03 23:58:19 -0400

    Add files via upload

---
+++ description.py
@@ -0,0 +1,14 @@
+purge_brief="Delete messages"
+purge_description="Takes 1 arg: number of messages to be deleted (excluding your own ;D). Must be an Admin."
+
+ping_brief="Hello?"
+ping_description="Will respond pong if online"
+
+makerole_brief="still under development"
+makerole_description="Creates a non existing role. Still needs improvement"
+
+inspire_brief="Uptime = now"
+inspire_description="Will fetch inspiration from zenquotes API\n inspired by: zenquotes"
+
+comp_brief="Links to Competition papers. See full description..."
+comp_description="Takes 2 args: Type (currently supports Euclid and Euclid-solution), publication year"
